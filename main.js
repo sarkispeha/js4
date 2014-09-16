15810aa030491d8c7310f051f3760be4446d02a1
Sarkis Peha <sarkispeha@gmail.com>
2014-09-15 22:15:05 -0600

    set arrays for volunteers and victims, loops now store name, phone, and address for newVictim and newVolunteer objects

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var Victims = [];
+var victims = [];
 
 var totalVictims = prompt('How many people got their shizzy wrecked?');
 
@@ -8,10 +8,27 @@ for (var i = 0; i < totalVictims; i++) {
 		phonenum: prompt('Phone?'),
 		street: prompt('Address?'),
 	}
+	victims.push(newVictim);
 };
+
+
+console.log(victims);
+
+var volunteers = [];
 
 var totalVolunteers = prompt('How many people are ready to help?');
 
-var listVictims = Victims["name"]
+for (var i = 0; i<totalVolunteers; i++){
+	var newVolunteer = {
+		name: prompt('Name?'),
+		phonenum: prompt('Phone?'),
+		street: prompt('Address?'),
+	}
+	volunteers.push(newVolunteer);
+};
 
-alert(totalVictims + totalVolunteers)
+console.log(volunteers);
+
+// var listVictims = Victims["name"]
+
+// alert('There are ' + totalVictims + ' victims and ' + totalVolunteers + ' volunteers!');
